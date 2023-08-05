8cde1e3edc8bf58e4f2ce5a19f4442f06a793954
jmr10200 <jmr10200@gmail.com>
2023-08-05 14:52:24 +0900

    deploy test

---
+++ src/routes/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./About.css";
+
+function About() {
+    return (
+        <div className="about__container">
+            <span>
+                "영화~~~설명~~~~"
+            </span>
+            <span> - react 학습 2023</span>
+        </div>
+    );
+}
+
+export default About;
